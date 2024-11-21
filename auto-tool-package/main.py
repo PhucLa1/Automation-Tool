b79fd534c303aeab3132ffb8217070c08320bc6d
hoang <hoang212610510@lms.utc.edu.vn>
2024-11-22 03:42:13 +0700

    Thêm Approve and Decline automation trong contract-approval

--- auto-tool-package/main.py
+++ auto-tool-package/main.py
@@ -2,20 +2,19 @@ import os
 
 import eel
 import pyautogui
-from auth_admin import login_admin
 import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from helper import read_json_file
-from post_api import add_position
+from post_api import add_position, login_admin, add_contract, approve_decline
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # Lấy đường dẫn tới file hiện tại
 admin_path = os.path.join(parent_dir, "auth", "admin")
 
 
 eel.init('../web')
 @eel.expose
-def login():
+def login_admin_main():
     result = login_admin(WebDriverWait, EC, time, By, admin_path)
     return result
 
@@ -24,6 +23,15 @@ def add_position_main():
     result = add_position(WebDriverWait, EC, time, By, admin_path)
     return result
 
+@eel.expose
+def add_contract_main():
+    result = add_contract(WebDriverWait, EC, time, By, admin_path)
+    return result
+
+@eel.expose
+def approve_decline_main():
+    result = approve_decline(WebDriverWait, EC, time, By, admin_path)
+    return result
 
 @eel.expose
 def get_tool_data():
--- web/script.js
+++ web/script.js
@@ -1,6 +1,6 @@
 async function loginAdmin() {
     try {
-        const result = await eel.login()(); // Gọi hàm login từ Python
+        const result = await eel.login_admin_main(); // Gọi hàm login từ Python
     } catch (error) {
         console.error("Error:", error);
     }
@@ -9,6 +9,22 @@ async function loginAdmin() {
 async function addPosition() {
     try {
         const result = await eel.add_position_main(); // Gọi hàm login từ Python
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+async function addContract() {
+    try {
+        const result = await eel.add_contract_main(); // Gọi hàm login từ Python
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+async function approveDecline() {
+    try {
+        const result = await eel.approve_decline_main(); // Gọi hàm login từ Python
     } catch (error) {
         console.error("Error:", error);
     }
