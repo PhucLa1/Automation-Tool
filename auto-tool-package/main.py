3f91563de6f6ac07c43bc9e27a84f57568b648c7
Toan211203 <toancoi123452022@gmail.com>
2024-12-02 21:24:57 +0700

    add bonus, tax rate, deduction test

--- auto-tool-package/main.py
+++ auto-tool-package/main.py
@@ -8,7 +8,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from helper import read_json_file
-from post_api import add_position
+from post_api import add_position, add_bonus, add_deduction, add_tax_rate, add_bonus_error
+
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # Lấy đường dẫn tới file hiện tại
 admin_path = os.path.join(parent_dir, "auth", "admin")
 
@@ -24,6 +25,24 @@ def add_position_main():
     result = add_position(WebDriverWait, EC, time, By, admin_path)
     return result
 
+@eel.expose
+def add_bonus_main():
+    result = add_bonus(WebDriverWait, EC, time, By, admin_path)
+    return result
+@eel.expose
+def add_bonus_error_main():
+    result = add_bonus_error(WebDriverWait, EC, time, By, admin_path)
+    return result
+
+@eel.expose
+def add_deduction_main():
+    result = add_deduction(WebDriverWait, EC, time, By, admin_path)
+    return result
+
+@eel.expose
+def add_tax_rate_main():
+    result = add_tax_rate(WebDriverWait, EC, time, By, admin_path)
+    return result
 
 @eel.expose
 def get_tool_data():
